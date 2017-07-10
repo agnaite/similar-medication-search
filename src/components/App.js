da5abeae86605b5471913732ba755e4ce09a91b7
Agne Klimaite <agnaite@me.com>
2017-07-10 15:21:07 -0700

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Navbar, Button } from 'react-bootstrap';
+import Header from './Header';
 import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Navbar, Grid } from 'react-bootstrap';
+
+import logo from '../images/drugs.png';
+import '../css/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <Navbar>
+        <Grid>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <img className="navbar-logo" src={logo} alt="medicine-icon" />
+              <a href="/">SearchMed</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Grid>
+      </Navbar>
+    )
+  }
+}
+
+export default Header;
