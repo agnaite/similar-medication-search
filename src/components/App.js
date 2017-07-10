a1c9075ca2d92adc8012e84fc47fe28d9ad7f96d
Agne Klimaite <agnaite@me.com>
2017-07-10 13:59:37 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Navbar, Button } from 'react-bootstrap';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
